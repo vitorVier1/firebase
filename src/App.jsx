792a9278cf501db1e97c5ec0d77f03ce83602f48
vitorVier1 <vitor.vier@sou.unijui.edu.br>
2024-10-16 16:04:26 -0300

    add_db_connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
-
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+import { db } from './dbConnection';
+
+import { useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore'
 
 import './App.css';
 
@@ -12,17 +15,24 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleRegister() {
-    return toast.success('Conta cadastrada com sucesso!')
-  }
-
-  function erro() {
-    toast.warn('error')
+  async function handleRegister() {
+    await addDoc(collection(db, "cadastros"), {
+      name: name,
+      lastName: lastName,
+      email: email,
+      password: password
+    })
+    .then(() => {
+      console.log('Conta cadastrada com sucesso!')
+    })
+    .catch(() => {
+      console.log('Erro')
+    })
   }
 
   return (
     <div className="container-app">
-      <ToastContainer autoClose={5000} />
+      <ToastContainer autoClose={4500} />
 
       <form className='form'>
         <div className='inputs'>
@@ -31,29 +41,37 @@ function App() {
             type='text' 
             className='input-nome'
             placeholder='Digite seu nome'
+            value={name}
+            onChange={ (e) => setName(e.target.value)}
           />
 
           <input 
             type='text' 
             className='input-sobrenome'
             placeholder='Digite seu sobrenome'
+            value={lastName}
+            onChange={ (e) => setLastName(e.target.value)}
           />
           
           <input 
             type='email' 
             className='input-email'
             placeholder='Digite seu e-mail'
+            value={email}
+            onChange={ (e) => setEmail(e.target.value)}
           />
           
           <input 
             type='password' 
             className='input-senha'
             placeholder='Digite sua senha'  
+            value={password}
+            onChange={ (e) => setPassword(e.target.value)}
           />
 
           <div className='buttons'>
-            <input type='reset' value='Limpar' onClick={erro}/>
-            <input type='button' value='Enviar' onClick={handleRegister}/>
+            <button type='reset'>Reset</button>
+            <button onClick={handleRegister} className='send'>Enviar</button>
           </div>
         </div>
       </form>
